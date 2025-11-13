092421f25842b9ac4de0f4acd65e766dd57b2adb
Nothemba Tsheme <tnothemba@gmail.com>
2025-11-13 18:07:59 +0200

    completed extent report class

--- src/test/java/ExtentReports/ReportsManager.java
+++ src/test/java/ExtentReports/ReportsManager.java
@@ -4,12 +4,23 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class ReportsManager {
 
     private static String reportsDir = System.getProperty("user.dir")+ "/Reports/SourceDemo.html";
     private static ExtentReports extentReports;
     private static ExtentSparkReporter extentSpartReporter;
-
+    public static ExtentReports extentReports(){
+        extentReports = new ExtentReports();
+        extentSpartReporter = new ExtentSparkReporter(new File(reportsDir));
+        extentReports.attachReporter(extentSpartReporter);
+        extentSpartReporter.config().setDocumentTitle("Extent Reports");
+        extentSpartReporter.config().setReportName("Source demo");
+        extentReports.setSystemInfo("OS", System.getProperty("os.name"));
+        extentReports.setSystemInfo("execution machine",System.getProperty("user.name"));
+        return extentReports;
+    }
 
     @Test
     public void test(){
--- src/test/java/Tests/myTests.java
+++ src/test/java/Tests/myTests.java
@@ -5,70 +5,79 @@ import org.testng.annotations.Test;
 
 public class myTests extends Base{
     @Test
-    public void DisplayHomePageTest(){
+    public void DisplayHomePageTest() {
         homePage.verifyHomepageIsDisplayed();
     }
-    @Test(priority = 1)
 
-    public void ClickLeaningMaterialButtonTest(){
+    @Test(dependsOnMethods = {"DisplayHomePageTest"})
+    public void ClickLeaningMaterialButtonTest() {
         homePage.ClickLeaningMaterialButton();
     }
-    @Test(priority = 2)
-    public void verifyLoginPageIsDisplayedTest(){
+
+    @Test(dependsOnMethods = {"ClickLeaningMaterialButtonTest"})
+    public void verifyLoginPageIsDisplayedTest() {
         login.verifyLoginPageIsDisplayed();
     }
 
-    @Test(priority = 3)
-    public void EnterEmailTest(){
+    @Test(dependsOnMethods = {"verifyLoginPageIsDisplayedTest"})
+    public void EnterEmailTest() {
         login.EnterEmail("nothemba@gmail.com");
     }
-    @Test (priority = 4)
-    public void EnterPasswordTest(){
+
+    @Test(dependsOnMethods = {"EnterEmailTest"})
+    public void EnterPasswordTest() {
         login.EnterPassword("12345678");
     }
-    @Test(priority = 5)
-    public void ClickLoginButton(){
+
+    @Test(dependsOnMethods = {"EnterPasswordTest"})
+    public void ClickLoginButton() {
         login.ClickLoginButton();
     }
 
-    @Test(priority = 6)
-    public void welcomeMessageTest(){
+    @Test(dependsOnMethods = {"ClickLoginButton"})
+    public void welcomeMessageTest() {
         learningPage.verifyWelcomeMessage();
     }
-    @Test(priority = 7)
-    public void clickWebAdvancedTest(){
+
+    @Test(dependsOnMethods = {"welcomeMessageTest"})
+    public void clickWebAdvancedTest() {
         learningPage.clickWebAdvanced();
     }
-    @Test(priority = 8)
-    public  void SelectDeviceTest(){
+
+    @Test(dependsOnMethods = {"clickWebAdvancedTest"})
+    public void SelectDeviceTest() {
         webAutomationAdvanced.deviceType();
     }
-    @Test(priority = 9)
-    public void selectDeviceTypeTest(){
+
+    @Test(dependsOnMethods = {"SelectDeviceTest"})
+    public void selectDeviceTypeTest() {
         webAutomationAdvanced.Brand();
     }
 
-    @Test(priority = 10)
-    public void selectStorageSizeTest(){
+    @Test(dependsOnMethods = {"selectDeviceTypeTest"})
+    public void selectStorageSizeTest() {
         webAutomationAdvanced.selectStorageSize();
     }
-    @Test(priority = 11)
-    public void selectColorTest(){
+
+    @Test(dependsOnMethods = {"selectStorageSizeTest"})
+    public void selectColorTest() {
         webAutomationAdvanced.selectColor();
     }
-    @Test(priority = 12)
-    public void enterQuantityTest(){
+
+    @Test(dependsOnMethods = {"selectColorTest"})
+    public void enterQuantityTest() {
         webAutomationAdvanced.EnterQuantity();
     }
-    @Test(priority = 13)
-    public void enterAddressTest(){
+
+    @Test(dependsOnMethods = {"enterQuantityTest"})
+    public void enterAddressTest() {
         webAutomationAdvanced.enterAddress("123 Protea Street");
     }
-    @Test(priority = 14)
-    public void nextButtonTest(){
+
+    @Test(dependsOnMethods = {"enterAddressTest"})
+    public void nextButtonTest() {
         webAutomationAdvanced.nextButton();
     }
-
 
 
 
